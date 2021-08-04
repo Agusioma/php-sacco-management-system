8a3c9dccd0ffb62ce1b43aa3a63bc77967993b5e
Agusioma <terryterrence200@gmail.com>
2021-08-04 15:03:10 +0300

    daraja fix

--- sacco/backend/tester.php
+++ sacco/backend/tester.php
@@ -3,86 +3,113 @@
   date_default_timezone_set('Africa/Nairobi');
 
   //if($_SERVER["REQUEST_METHOD"] == "POST"){
-          $telephone = 254702277060; //trim($_POST["number"]);
-          $cash = 21;//trim($_POST["amount"]);
+          $telephone = '254757153404'; //trim($_POST["number"]);
+          $cash = '21';//trim($_POST["amount"]);
   # access token
-              $consumerKey = 'ZDQ2HRlEglcqwijP5SDCYFVAh2KM9bXj'; //Fill with your app Consumer Key
-              $consumerSecret = 'nOxTzXoBC2nx8Y9M'; // Fill with your app Secret
 
-              # define the variales
-              # provide the following details, this part is found on your test credentials on the developer account
-              $BusinessShortCode = '174379';
-              $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
-              
-              /*
-                This are your info, for
-                $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
-                $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
-                TransactionDesc can be anything, probably a better description of or the transaction
-                $Amount this is the total invoiced amount, Any amount here will be 
-                actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
-                for developer/test accounts, this money will be reversed automatically by midnight.
-              */
-              
-              $PartyA = $telephone; // This is your phone number, 
-              $AccountReference = 'JUJA BASED COMMUNITY SACCO';
-              $TransactionDesc = 'Savings Deposit';
-              $Amount = $cash;
-            
-              # Get the timestamp, format YYYYmmddhms -> 20181004151020
-              $Timestamp = date('YmdHis');    
-              
-              # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
-              $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
+  # access token
+  $consumerKey = 'dm5kaoWPmYcHAX8sTjDKQobbNLIuw62F'; //Fill with your app Consumer Key
+  $consumerSecret = 'x8Vd1DA1f01iHalH'; // Fill with your app Secret
 
-              # header for access token
-              $headers = ['Content-Type:application/json; charset=utf8'];
+  # define the variales
+  # provide the following details, this part is found on your test credentials on the developer account
+  $BusinessShortCode = '174379';
+  $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+  
+  /*
+    This are your info, for
+    $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
+    $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
+    TransactionDesc can be anything, probably a better description of or the transaction
+    $Amount this is the total invoiced amount, Any amount here will be 
+    actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
+    for developer/test accounts, this money will be reversed automatically by midnight.
+  */
+  
+  $PartyA = $telephone; // This is your phone number,
+  $AccountReference = 'rr';
+  $TransactionDesc = 'rr';
+  $Amount = $cash;
+ 
+  # Get the timestamp, format YYYYmmddhms -> 20181004151020
+  $Timestamp = date('YmdHis');    
+  
+  # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+  $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
 
-                # M-PESA endpoint urls
-              $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
-              $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+  # header for access token
+  $headers = ['Content-Type:application/json; charset=utf8'];
 
-              # callback url
-              $CallBackURL = 'https://sacco.terrence-aluda.com/sacco/backend/callback_url.php';  
+    # M-PESA endpoint urls
+  $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
-              $curl = curl_init($access_token_url);
-              curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-              curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-              curl_setopt($curl, CURLOPT_HEADER, FALSE);
-              curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
-              $result = curl_exec($curl);
-              $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-              $result = json_decode($result);
-              $access_token = $result->access_token;  
-              curl_close($curl);
+  # callback url
+  $CallBackURL = 'https://sacco.terrence-aluda.com/sacco/backend/callback_url.php';
 
-              # header for stk push
-              $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
+  $curl = curl_init($access_token_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($curl, CURLOPT_HEADER, FALSE);
+  curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+  $result = curl_exec($curl);
+  $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+  $result = json_decode($result);
+  $access_token = $result->access_token;  
+  curl_close($curl);
 
-              # initiating the transaction
-              $curl = curl_init();
-              curl_setopt($curl, CURLOPT_URL, $initiate_url);
-              curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+  # header for stk push
+  $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
 
-              $curl_post_data = array(
-                //Fill in the request parameters with valid values
-                'BusinessShortCode' => $BusinessShortCode,
-                'Password' => $Password,
-                'Timestamp' => $Timestamp,
-                'TransactionType' => 'CustomerPayBillOnline',
-                'Amount' => $Amount,
-                'PartyA' => $PartyA,
-                'PartyB' => $BusinessShortCode,
-                'PhoneNumber' => $PartyA,
-                'CallBackURL' => $CallBackURL,
-                'AccountReference' => $AccountReference,
-                'TransactionDesc' => $TransaYourctionDesc
-              );
+  # initiating the transaction
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $initiate_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
 
-              $data_string = json_encode($curl_post_data);
-              curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-              curl_setopt($curl, CURLOPT_POST, true);
-              curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-              $curl_response = curl_exec($curl);
+  $curl_post_data = array(
+    //Fill in the request parameters with valid values
+    'BusinessShortCode' => $BusinessShortCode,
+    'Password' => $Password,
+    'Timestamp' => $Timestamp,
+    'TransactionType' => 'CustomerPayBillOnline',
+    'Amount' => $Amount,
+    'PartyA' => $PartyA,
+    'PartyB' => $BusinessShortCode,
+    'PhoneNumber' => $PartyA,
+    'CallBackURL' => $CallBackURL,
+    'AccountReference' => $AccountReference,
+    'TransactionDesc' => $TransactionDesc
+  );
+
+  $data_string = json_encode($curl_post_data);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+  $curl_response = curl_exec($curl);
 //}
+
+/*
+$ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest');
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer 5sUkIzFJrspfZTPmhL6YS36B0ynO',
+    'Content-Type: application/json'
+]);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, {
+    "BusinessShortCode": 174379,
+    "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjEwODAzMTc0MTU3",
+    "Timestamp": "20210803174157",
+    "TransactionType": "CustomerPayBillOnline",
+    "Amount": 1,
+    "PartyA": 254757153404,
+    "PartyB": 174379,
+    "PhoneNumber": 254757153404,
+    "CallBackURL": "https://sacco.terrence-aluda.com/sacco/backend/callback_url.php",
+    "AccountReference": "CompanyXLTD",
+    "TransactionDesc": "Payment of X" 
+  });
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response     = curl_exec($ch);
+curl_close($ch);
+echo $response;*/
 ?>
