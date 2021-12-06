e43302363eb62db8f2a9a00f3b588f8abcb5c62f
Agusioma <terryterrence200@gmail.com>
2021-12-06 18:31:13 +0300

    minor

--- sacco/backend/connection.php
+++ sacco/backend/connection.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'www.sacco.terrence-aluda.com');
-define('DB_USERNAME', 'terrence_admin');
-define('DB_PASSWORD', 'TerrAld$$254!');
-define('DB_NAME', 'terrence_jbcs');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
